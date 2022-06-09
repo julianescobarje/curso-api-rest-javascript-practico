8c0334aa3a899baf1365dfca7582b06cab899e2f
Julian Escobar <escobarjulian347@gmail.com>
2022-06-09 16:47:03 -0500

    feat: :sparkles: add trending preview section

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
-console.log('Hello, world')
+const API_URL = 'https://api.themoviedb.org/3'
+const IMAGES_W300_URL = 'https://image.tmdb.org/t/p/w300'
+
+async function getTrendingMoviesPreview() {
+	const res = await fetch(`${API_URL}/trending/movie/day?api_key=${API_KEY}`)
+	const data = await res.json()
+
+	const movies = data.results
+
+	console.log({ data, movies })
+
+	const trendingPreviewMoviesContainer = document.querySelector(
+		'#trendingPreview .trendingPreview-movieList'
+	)
+
+	movies.forEach(movie => {
+		const movieContainer = document.createElement('div')
+		movieContainer.classList.add('movie-container')
+
+		const movieImg = document.createElement('img')
+		movieImg.classList.add('movie-img')
+		movieImg.setAttribute('alt', movie.title)
+		movieImg.setAttribute('src', `${IMAGES_W300_URL}${movie.poster_path}`)
+
+		movieContainer.appendChild(movieImg)
+		trendingPreviewMoviesContainer.appendChild(movieContainer)
+	})
+}
+
+getTrendingMoviesPreview()
